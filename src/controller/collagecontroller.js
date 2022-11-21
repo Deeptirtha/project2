4214312675327a466fc00dec63d0d8a1a626566e
Deeptirtha <tirtha1111222@gmail.com>
2022-11-21 21:28:29 +0530

    nov 21

--- src/controller/collagecontroller.js
+++ src/controller/collagecontroller.js
@@ -6,22 +6,26 @@ const createcollege=async (req,res)=>{
     
     try{
         let data =req.body
-          data.name=req.body.name.trim()
-          data.fullName=req.body.fullName.trim()
+         
+          let newarr=["name","fullName","logoLink"]
         if(Object.keys(data)==0){return res.status(400).send({status:false,msg:"body is empty can not creat any thing"})}
-        if(!data.name)return  res.status(400).send({status:false,msg:"name is empty input valid name"})
-        if(!data.fullName)return  res.status(400).send({status:false,msg:"fullName is empty input valid full name"})
-        if(!data.logoLink)return  res.status(400).send({status:false,msg:"logoLink is empty provide valid Logo Link"})
-
-        if (validName.test(data.name)) return res.status(400).send({ status: false, message: "please provide valid name " })
-        if (validName.test(data.fullName)) return res.status(400).send({ status: false, message: "please provide valid fullName" })
-        if (!validUrl.test(data.logoLink)) return res.status(400).send({ status: false, message: "please provide valid logoLink" })
+        for(field of newarr){
+            if(!data[field])return  res.status(400).send({status:false,msg:`${field} is empty input valid name`})
+        }
+        data.name=req.body.name.trim()
+        data.fullName=req.body.fullName.trim()
+        if (validName.test(data.name)) return res.status(400).send({ status: false, msg: "please provide valid name " })
+        if (validName.test(data.fullName)) return res.status(400).send({ status: false, msg: "please provide valid fullName" })
+        if (!validUrl.test(data.logoLink)) return res.status(400).send({ status: false, msg: "please provide valid logoLink" })
 
         let college=await collegeModel.findOne({name:data.name})
         if(college)return res.status(400).send({status:false,msg:"college already exist"})
 
         let creatcollege=await collegeModel.create(data)
-        res.status(201).send({status:true,msg:creatcollege})
+        let collegedetails=creatcollege.toObject();
+
+        ["_id","createdAt","isDeleted","__v","updatedAt"].forEach(x=>delete collegedetails[x])
+        res.status(201).send({status:true,Data:collegedetails})
     }
     catch(err){
         res.status(500).send({status:false,msg:err.message})
@@ -30,18 +34,18 @@ const createcollege=async (req,res)=>{
 
 const getintern = async function (req, res) {
     try {
-      let collagename = req.query;
+      let collagename = req.query
+      if(!collagename.hasOwnProperty('name') || Object.keys(collagename).length>1){return res.status(400).send({status:false,msg:"enter valid query"})}
       collagename.isdeleted=false
 
-      console.log(collagename)
-       
       if(!collagename){return res.status(400).send({status:false,msg:"no query is present"})}
       let result = await collegeModel.findOne(collagename).select({name:1,fullName:1,logoLink:1})
+     
       if (!result) {return res.status(404).send({ status: false, msg: "No collage found" }) }
-      let intern=await internModel.find({collegeId:result._id}).select({ name: 1, email: 1, mobile: 1 })
-      if (!intern) return res.status(404).send({ status: false, message: "No interns available for this college" })
+      let allintern=await internModel.find({collegeId:result._id})
+      if (!allintern) return res.status(200).send({ status: true, message: "No interns available for this college",Data:{name:result.name,fullName:result.fullName,logoLink:result.logoLink,intern:allintern}})
        
-      res.status(200).send({status:true,data:{name:result.name,fullName:result.fullName,logoLink:result.logoLink,interndata:intern}}) 
+      res.status(200).send({status:true,Data:{name:result.name,fullName:result.fullName,logoLink:result.logoLink,intern:allintern}}) 
 
 
  } catch (err) {
--- src/controller/interncontroller.js
+++ src/controller/interncontroller.js
@@ -7,14 +7,15 @@ const createIntern=async (req,res)=>{
     
     try{
         let data =req.body
-          data.name=req.body.name.trim()
-          data.collegeName=req.body.collegeName.trim()
+         
           
         if(Object.keys(data)==0){return res.status(400).send({status:false,msg:"body is empty can not creat any thing"})}
-        if(!data.name)return  res.status(400).send({status:false,msg:"name is empty input valid name"})
-        if(!data.email)return  res.status(400).send({status:false,msg:"Email is empty input valid Email"})
-        if(!data.mobile)return  res.status(400).send({status:false,msg:"Mobile No is empty input valid Mobile No"})
-        if(!data.collegeName)return  res.status(400).send({status:false,msg:"college name is empty input valid college name"})
+        let newarr=["name","email","mobile","collegeName"]
+        for(field of newarr){
+            if(!data[field])return  res.status(400).send({status:false,msg:`${field} is empty input valid name`})}
+
+            data.name=req.body.name.trim()
+            data.collegeName=req.body.collegeName.trim()
 
         if (validName.test(data.collegeName)) return res.status(400).send({ status: false, message: "please provide valid name" })
         if (validName.test(data.name)) return res.status(400).send({ status: false, message: "please provide valid name" })
@@ -30,8 +31,11 @@ const createIntern=async (req,res)=>{
         let students = await interModel.findOne({ $or: [{ email: data.email }, { mobile: data.mobile }] });
         if (students) return res.status(400).send({ status: false, message: "Email or Mobile number already exist" })
     
-        let InterData = await interModel.create({name:data.name,email:data.email,mobile:data.mobile,collegeId:data.collegeId})
-        res.status(201).send({ status: true, message: "Account created successfully", data: InterData })
+        let intern = await interModel.create({name:data.name,email:data.email,mobile:data.mobile,collegeId:data.collegeId})
+        let InterData=intern.toObject();
+
+        ["_id","isdeleted","__v"].forEach(x=>delete InterData[x])
+        res.status(201).send({ status: true, Data: InterData })
     }
     catch(err){
         res.status(500).send({status:false,msg:err.message})
