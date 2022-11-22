b6bb62f1895de1976d7ea3046dc3f96f872f24df
Deeptirtha <tirtha1111222@gmail.com>
2022-11-22 22:56:13 +0530

    done

--- src/controller/collagecontroller.js
+++ src/controller/collagecontroller.js
@@ -39,7 +39,7 @@ const getintern = async function (req, res) {
        
         if (!college.hasOwnProperty('collegeName') || Object.keys(college).length > 1) { return res.status(400).send({ status: false, msg: "enter valid query" }) }
         if (!college) { return res.status(400).send({ status: false, msg: "no query is present" }) }
-     
+        
         let result = await collegeModel.findOne({name:college.collegeName,isDeleted:false}).select({ name: 1, fullName: 1, logoLink: 1 }).lean()
         if (!result) { return res.status(404).send({ status: false, msg: "No collage found" }) }
         let allintern = await internModel.find({ collegeId: result._id, isdeleted: false }).select({ name: 1, email: 1, mobile: 1 })
